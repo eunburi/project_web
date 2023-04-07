bf1f0ada6229d818912eafce538fdc47466dd9f7
eunburi <finestar23@naver.com>
2023-04-07 18:20:50 +0900

    submenu

---
+++ cafe12/js/main-js.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+    //디지털 시계
+    setInterval(setwatch, 1000);
+
+    function setwatch(){
+        const date = new Date();
+        let now = date.toLocaleTimeString(); //시간 형식
+        document.getElementById("demo").innerHTML = now;
+    }
+
+    //배경 이미지 change
+    let picture = ["./images/header1.jpg","./images/header2.jpg","./images/header3.jpg"];
+    let imgIdx = 0;
+
+    showPicture(); //함수 호출
+
+    function showPicture(){
+        let img = document.getElementById("pic");
+        img.src = picture[imgIdx];
+        imgIdx++;
+
+        if(imgIdx == picture.length){
+            imgIdx = 0;
+        }
+        setTimeout(showPicture,2000); //콜백함수
+}}
